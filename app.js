3dcbcf5b94a5947a5ba1f3c7246661c2ef12bdcf
pareen-web <66211509+pareen-web@users.noreply.github.com>
2020-10-03 14:15:53 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello World From DEV-OPS COMMUNITY And cloud community !!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
